a7fc71519755a5a3fd95c08089fa778f2e4c6eb7
sirdan <stanford303@gmail.com>
2022-04-27 11:50:27 -0700

    picked cheese

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ const item1 = document.getElementById('soda');
 item1.addEventListener('click', () => {
     item1.classList.toggle('picked');
 });
+
+const item2 = document.getElementById('cheese');
+
+item2.addEventListener('click', () => {
+    item2.classList.toggle('picked');
+});
